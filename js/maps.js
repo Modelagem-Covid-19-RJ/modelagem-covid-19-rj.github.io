2399dfd743f9fcbdbcd522b621549a538a73f78a
GilWindows <gilsmneto@gmail.com>
2020-05-02 17:31:32 -0300

    layout

--- js/maps.js
+++ js/maps.js
@@ -4,6 +4,7 @@ var map = L.map('map', {
   zoom: 11
 });
 
+
 var CartoDB_Positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
 	subdomains: 'abcd',
