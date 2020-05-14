5dbd6878aa1ed0673b5a991d41466abdcbaf1be1
hollebenthiago <hollebenthiago@gmail.com>
2020-05-14 17:16:56 -0300

    site com htmls do plotly
    Co-Authored-by: Gil Miranda <gilsmneto@gmail.com>
    Co-Authored-by: Cynthia Herkenhoff <cynthia.herkenhoff@gmail.com>
    Co-Authored-by: Beatriz Farah <biafarah28@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,92 +1,54 @@
 $( document ).ready(function() {
-    $( ".mapa-control-tipo li:nth-child(1)" ).click(function() {
-		if ($('.mapa-control-tipo li:nth-child(1)').hasClass('tipo-ativo')){
+    var tipo = 'confirmados';
+    var qualidade = 'acum';
+    var newUrl = 'includes/series_temporais_bairros_' + tipo + '_' + qualidade + '.html';
+    var titulo_tipo = 'Casos';
+    var titulo_qualidade = 'acumulados';
+    $('.plot').attr('src', newUrl);
+    
+    $(".tipo a").click(function() {
+        tipo = $(this).attr("tipo");
+        newUrl = 'includes/series_temporais_bairros_' + tipo + '_' + qualidade + '.html';
+        if ($(this).attr('tipo') == 'confirmados'){
+            $('.titulo-tipo').html('Casos');
+        }
+
+        else {
+            $('.titulo-tipo').html('Óbitos');
+        }
+        $('.plot').attr('src', newUrl);
+        if ($(this).hasClass('botao-ativo')){
 			console.log('1')
 		}	
 
 		else {
-			
-			$('.tipo-conf').addClass('tipo-ativo');
-			$('.mapa-control').addClass('mapa-control-atv-conf');
-			$('.mapa-container').addClass('mapa-container-atv-conf');
+            
+            $('.tipo .botao-ativo').removeClass('botao-ativo');
+            $(this).addClass('botao-ativo');
+            
+            console.log('2')
+        }
+        return false;
+    });
 
-			$('.tipo-obit').removeClass('tipo-ativo');
-			$('.mapa-control').removeClass('mapa-control-atv-obit');
-			$('.mapa-container').removeClass('mapa-container-atv-obit');
-			console.log('2')
-			
-			if ($(".mapa-control-loc li:nth-child(1)").hasClass('loc-ativo')){
-				
-				$('geoplot').attr('src', 'includes/Bairros_acumulados.html');
-				console.log('3')
-			}
-			
-			else if ($(".mapa-control-loc li:nth-child(2)").hasClass('loc-ativo')){
-				
-				$('#geoplot').attr('src', 'includes/AP_acumulados.html');
-				console.log('4')
-			}	
-		}
-	});
-});
+    $(".qualidade a").click(function() {
+        qualidade = $(this).attr("qualidade");
+        newUrl = 'includes/series_temporais_bairros_' + tipo + '_' + qualidade + '.html';
+        $('.plot').attr('src', newUrl);
 
-$( document ).ready(function() {
-    $( ".mapa-control-tipo li:nth-child(2)" ).click(function() {
-		if ($(".mapa-control-tipo li:nth-child(2)").hasClass('tipo-ativo')){
-			console.log('f')
+        if ($(this).hasClass('botao-ativo')){
+			console.log('1')
 		}	
 
 		else {
-			
-			console.log('g')
-			$('.tipo-obit').addClass('tipo-ativo');
-			$('.mapa-control').addClass('mapa-control-atv-obit');
-			$('.mapa-container').addClass('mapa-container-atv-obit');
-			
-			$('.tipo-conf').removeClass('tipo-ativo');
-			$('.mapa-control').removeClass('mapa-control-atv-conf');
-			$('.mapa-container').removeClass('mapa-container-atv-conf');
-
-			if ($(".mapa-control-loc li:nth-child(1)").hasClass('loc-ativo')){
-				
-				$('#geoplot').attr('src', 'includes/Bairros_acumulados.html');
-				console.log('h')
-			}
-
-			else if ($(".mapa-control-loc li:nth-child(2)").hasClass('loc-ativo')){
-				
-				$('#geoplot').attr('src', 'includes/AP_acumulados.html');
-				console.log('i')
-			}
-		}
-	});
-});
+            
+            $('.qualidade .botao-ativo').removeClass('botao-ativo');
+            $(this).addClass('botao-ativo');
+            
+            console.log('2')
+        }
+        return false;
+    });
 
 
-$( document ).ready(function() {
-    $( ".mapa-control-loc li:nth-child(1)" ).click(function() {
-		if ($(".mapa-control-loc li:nth-child(1)").hasClass('loc-ativo')){
-			console.log('a')
-		}	
-		else {
-			console.log('B')
-			$('.loc-bairros').addClass('loc-ativo-bairros');
-		
-			$('loc-ap').removeClass('loc-ativo-ap');
-		}
-	});
-});
-
-$( document ).ready(function() {
-    $( ".mapa-control-loc li:nth-child(2)" ).click(function() {
-		if ($(".mapa-control-loc li:nth-child(2)").hasClass('loc-ativo-ap')){
-			console.log('c')
-		}	
-		else {
-			console.log('d')
-			$('.loc-ap').addClass('loc-ativo-ap');
-		
-			$('loc-bairros').removeClass('loc-ativo-bairros');
-		}
-	});
-});
+})
