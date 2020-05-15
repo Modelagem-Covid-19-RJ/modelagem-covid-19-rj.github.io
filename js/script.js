67165ad412118cfe27d427b351b1c35c76d4a8d5
beafarah <63371380+beafarah@users.noreply.github.com>
2020-05-14 22:58:27 -0300

    colocamos geoplot (falam obitos e atualizar geoplots)
    Co-Authored-By: hollebenthiago <hollebenthiago@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 $( document ).ready(function() {
     var tipo = 'confirmados';
     var qualidade = 'acum';
-    var newUrl = 'includes/series_temporais_bairros_' + tipo + '_' + qualidade + '.html';
+    var plot_estilo = 'grafico';
+    var newUrl = 'includes/' + plot_estilo + '_bairros_' + tipo + '_' + qualidade + '.html';
     var titulo_tipo = 'Casos';
     var titulo_qualidade = 'acumulados';
+    
     $('.plot').attr('src', newUrl);
     
     $(".tipo a").click(function() {
         tipo = $(this).attr("tipo");
-        newUrl = 'includes/series_temporais_bairros_' + tipo + '_' + qualidade + '.html';
+        newUrl = 'includes/' + plot_estilo + '_bairros_' + tipo + '_' + qualidade + '.html';
         if ($(this).attr('tipo') == 'confirmados'){
             $('.titulo-tipo').html('Casos');
         }
@@ -33,7 +35,7 @@ $( document ).ready(function() {
 
     $(".qualidade a").click(function() {
         qualidade = $(this).attr("qualidade");
-        newUrl = 'includes/series_temporais_bairros_' + tipo + '_' + qualidade + '.html';
+        newUrl = 'includes/' + plot_estilo + '_bairros_' + tipo + '_' + qualidade + '.html';
         $('.plot').attr('src', newUrl);
 
         if ($(this).hasClass('botao-ativo')){
@@ -49,6 +51,27 @@ $( document ).ready(function() {
         }
         return false;
     });
+
+    $(".plot-estilo a").click(function() {
+        plot_estilo = $(this).attr("plot-estilo");
+        newUrl = 'includes/' + plot_estilo + '_bairros_' + tipo + '_' + qualidade + '.html';
+        $('.plot').attr('src', newUrl);
+
+        if ($(this).hasClass('botao-ativo')){
+			console.log('1')
+		}	
+
+		else {
+            
+            $('.plot-estilo .botao-ativo').removeClass('botao-ativo');
+            $(this).addClass('botao-ativo');
+            
+            console.log('2')
+        }
+        return false;
+    });
+
+    
 
 
 })
