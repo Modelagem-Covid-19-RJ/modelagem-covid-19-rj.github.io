38632c72b11c6b3fac106a94ed00e73f81c0404a
Gil <gilsmneto@gmail.com>
2020-05-18 08:28:57 -0300

    Novos Icones para a SideNav

--- assets/js/behaviors.js
+++ assets/js/behaviors.js
@@ -1,9 +1,9 @@
 
+// Local dos arquivos
+var url_nav = "https://raw.githubusercontent.com/modelagemcovid19/modelagemcovid19.github.io/dev/navigation.html";
+
+
+// Carregar via AJAX o arquivo que gera a barra de navegação
 $(function(){
-    $("#mc-vert-navigation").load("https://modelagemcovid19.github.io/", function( response, status, xhr ) {
-        if ( status == "error" ) {
-          var msg = "Sorry but there was an error: ";
-          $( "#error" ).html( msg + xhr.status + " " + xhr.statusText );
-        }
-      })
+    $("#sidenav-main").load(url_nav);
 });
