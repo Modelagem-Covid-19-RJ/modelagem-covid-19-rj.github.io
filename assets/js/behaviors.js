c132cb68c8d53a5029c215270894b428e03e11e9
Gil <gilsmneto@gmail.com>
2020-05-22 06:49:12 -0300

    JSON para os boxes estatisticos

--- assets/js/behaviors.js
+++ assets/js/behaviors.js
@@ -1,7 +1,7 @@
 
 // Local dos arquivos
 var url_nav = "https://raw.githubusercontent.com/modelagemcovid19/modelagemcovid19.github.io/dev/navigation.html";
-
+var url_estat = "https://raw.githubusercontent.com/Modelagem-Covid-19-RJ/modelagem-covid-19-rj.github.io/master/dados-estat.json";
 
 // Carregar via AJAX o arquivo que gera a barra de navegação
 $(function(){
@@ -9,6 +9,15 @@ $(function(){
 });
 
 
-function makeplot(tipo, idDiv) {
-    Plotly.d3.csv(dataFiles[tipo], function(data){ callbackFuncs[tipo](data, idDiv) } );
+function readJson() {
+    Plotly.d3.json(url_estat, function(data){ updateEstatCards(data) } );
   };
+
+  function updateEstatCards(data) {
+      $('#dados_estat_confirmados').text(data['MRJ']['Municipio']['Confirmados']);
+      $('#dados_estat_obitos').text(data['MRJ']['Municipio']['Óbitos']);
+      $('#dados_estat_recuperados').text(data['MRJ']['Municipio']['Recuperados']);
+      $('#dados_estat_mortalidade').text(data['MRJ']['Municipio']['Mortalidade'] + '%');
+  }
+
+  readJson();
